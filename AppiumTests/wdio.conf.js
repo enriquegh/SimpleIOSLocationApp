d7a49fd369ee3b69a467c7f81908c6456bac0ad0
Enrique Gonzalez <enrique@saucelabs.com>
2019-06-09 18:39:37 -0700

    Run iOS 11 and 12.2 on Sauce US RDC on Travis Prior to this change, no CI tests were being run

--- AppiumTests/wdio.conf.js
+++ AppiumTests/wdio.conf.js
@@ -65,13 +65,13 @@ exports.config = {
         {
             testobject_api_key: process.env.SAUCE_RDC_API_KEY,
             platformName: "iOS",
-            // platformVersion: 12
+            platformVersion: 12.2
         },
-        // {
-        //     testobject_api_key: process.env.SAUCE_RDC_API_KEY,
-        //     platformName: "iOS",
-        //     platformVersion: 11
-        // }
+        {
+            testobject_api_key: process.env.SAUCE_RDC_API_KEY,
+            platformName: "iOS",
+            platformVersion: 11
+        }
 
 
     ],
