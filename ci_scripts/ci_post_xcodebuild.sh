c8ea3b3facb08527c114327453f657409f248915
Enrique Gonzalez <egonzalezh94@gmail.com>
2022-08-31 21:33:50 -0700

    change env var to point to actual .ipa file

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -15,7 +15,7 @@ then
     if [ $CI_XCODEBUILD_ACTION = 'archive' ]
     then
         echo "Uploading to SauceLabs"
-        PAYLOAD=$(printf "@\"%s\"" "$CI_ARCHIVE_PATH")
+        PAYLOAD=$(printf "@\"%s\"" "$CI_DEVELOPMENT_SIGNED_APP_PATH")
 
         # Upload ipa to Saucelabs
         curl --tlsv1.2 --tls-max 1.2 -v -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
@@ -30,7 +30,7 @@ then
         curl --tlsv1.2 --tls-max 1.2 -v -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
                   --request POST 'https://api.eu-central-1.saucelabs.com/v1/storage/upload' \
                   --form "payload=$PAYLOAD" \
-                  --form 'name="Health Connect.ipa"'
+                  --form 'name="SimpleLocationApp.ipa"'
 
         echo "-----------------------------------------------------------------------------------------------------------------"
 
