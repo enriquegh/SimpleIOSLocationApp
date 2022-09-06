e5164df672a4179ab747bde059545f86c1b3c679
Enrique Gonzalez <egonzalezh94@gmail.com>
2022-09-06 13:17:04 -0700

    try connecting via nc

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -19,62 +19,69 @@ then
         PAYLOAD=$(printf "@\"%s/SimpleLocationApp.ipa\"" "$CI_DEVELOPMENT_SIGNED_APP_PATH")
         echo $PAYLOAD
         # Upload ipa to Saucelabs
-        curl --tlsv1.2 --tls-max 1.2 -v -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
-                  --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
-                  --form "payload=$PAYLOAD" \
-                  --form 'name="SimpleLocationApp.ipa"'
-
-        echo "-----------------------------------------------------------------------------------------------------------------"
         
-        echo "Upload to EMEA"
-        # Upload ipa to Saucelabs
-        curl --tlsv1.2 --tls-max 1.2 -v -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
-                  --request POST 'https://api.eu-central-1.saucelabs.com/v1/storage/upload' \
-                  --form "payload=$PAYLOAD" \
-                  --form 'name="SimpleLocationApp.ipa"'
-
-        echo "-----------------------------------------------------------------------------------------------------------------"
-
-
-        echo "Curl Concurrency"
-        curl -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
-            --request GET "https://api.us-west-1.saucelabs.com/rest/v1.2/users/$SAUCE_USERNAME/concurrency" \
-            --header 'Content-Type: application/json'
-
-
-        echo "-----------------------------------------------------------------------------------------------------------------"
-
-        echo "HTTP trial"
-        curl -I http://api.us-west-1.saucelabs.com
+        printf "GET / HTTP/1.0\r\n\r\n" | nc api.us-west-1.saucelabs.com 80
         
-        echo "app.saucelabs.com"
-        curl -I http://app.saucelabs.com
+        printf "GET / HTTP/1.0\r\n\r\n" | nc api.eu-central-1.saucelabs.com 80
         
-        echo "ipconfig.me"
-        curl ifconfig.me
-        curl https://ifconfig.me
+        printf "GET / HTTP/1.0\r\n\r\n" | nc saucelabs.com 80
 
-        echo "Open SSL Version"
-        openssl version
-
-        echo "-----------------------------------------------------------------------------------------------------------------"
-        echo "Open SSL Info"
-        openssl s_client -servername api.us-west-1.saucelabs.com -connect api.us-west-1.saucelabs.com:443 < /dev/null 2>/dev/null
-        
-        echo "-----------------------------------------------------------------------------------------------------------------"
-        echo "Open SSL Info"
-        openssl s_client -servername saucelabs.com -connect saucelabs.com:443 < /dev/null 2>/dev/null
-        
-        echo "-----------------------------------------------------------------------------------------------------------------"
-        echo "Open SSL Info"
-        openssl s_client -servername api.eu-central-1.saucelabs.com -connect api.eu-central-1.saucelabs.com:443 < /dev/null 2>/dev/null
-
-        echo "-----------------------------------------------------------------------------------------------------------------"
-        echo "Saucelabs Website"
-        curl -vvvv https://saucelabs.com/rest/v1/info/counter
-
-        echo "-----------------------------------------------------------------------------------------------------------------"
-        echo "US West"
-        curl -vvvv https://api.us-west-1.saucelabs.com/rest/v1/info/counter
+#        curl --tlsv1.2 --tls-max 1.2 -v -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
+#                  --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
+#                  --form "payload=$PAYLOAD" \
+#                  --form 'name="SimpleLocationApp.ipa"'
+#
+#        echo "-----------------------------------------------------------------------------------------------------------------"
+#
+#        echo "Upload to EMEA"
+#        # Upload ipa to Saucelabs
+#        curl --tlsv1.2 --tls-max 1.2 -v -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
+#                  --request POST 'https://api.eu-central-1.saucelabs.com/v1/storage/upload' \
+#                  --form "payload=$PAYLOAD" \
+#                  --form 'name="SimpleLocationApp.ipa"'
+#
+#        echo "-----------------------------------------------------------------------------------------------------------------"
+#
+#
+#        echo "Curl Concurrency"
+#        curl -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
+#            --request GET "https://api.us-west-1.saucelabs.com/rest/v1.2/users/$SAUCE_USERNAME/concurrency" \
+#            --header 'Content-Type: application/json'
+#
+#
+#        echo "-----------------------------------------------------------------------------------------------------------------"
+#
+#        echo "HTTP trial"
+#        curl -I http://api.us-west-1.saucelabs.com
+#
+#        echo "app.saucelabs.com"
+#        curl -I http://app.saucelabs.com
+#
+#        echo "ipconfig.me"
+#        curl ifconfig.me
+#        curl https://ifconfig.me
+#
+#        echo "Open SSL Version"
+#        openssl version
+#
+#        echo "-----------------------------------------------------------------------------------------------------------------"
+#        echo "Open SSL Info"
+#        openssl s_client -servername api.us-west-1.saucelabs.com -connect api.us-west-1.saucelabs.com:443 < /dev/null 2>/dev/null
+#
+#        echo "-----------------------------------------------------------------------------------------------------------------"
+#        echo "Open SSL Info"
+#        openssl s_client -servername saucelabs.com -connect saucelabs.com:443 < /dev/null 2>/dev/null
+#
+#        echo "-----------------------------------------------------------------------------------------------------------------"
+#        echo "Open SSL Info"
+#        openssl s_client -servername api.eu-central-1.saucelabs.com -connect api.eu-central-1.saucelabs.com:443 < /dev/null 2>/dev/null
+#
+#        echo "-----------------------------------------------------------------------------------------------------------------"
+#        echo "Saucelabs Website"
+#        curl -vvvv https://saucelabs.com/rest/v1/info/counter
+#
+#        echo "-----------------------------------------------------------------------------------------------------------------"
+#        echo "US West"
+#        curl -vvvv https://api.us-west-1.saucelabs.com/rest/v1/info/counter
     fi
 fi
